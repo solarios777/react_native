d9be37f4c625827886165aef1b495fad5a97e365
solomon <tsegasolomon635@gmail.com>
2025-07-30 20:01:16 +0300

    navigating between screens by using navigation props

--- screens/AboutScreen.tsx
+++ screens/AboutScreen.tsx
@@ -3,11 +3,17 @@ import {
     Text,
     StyleSheet,
 } from 'react-native';
-
-export default function AboutScreen() {
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { Button } from 'react-native';
+import { ScreenProps } from './HomeScreen';
+export default function AboutScreen({ navigation }: ScreenProps) {
     return (
         <View style={styles.container}>
             <Text style={{fontSize: 20}}>About Screen</Text>
+            <Button 
+                            title='Go to About' 
+                            onPress={() => navigation.navigate('Home')}
+                        />
         </View>
     );
 }
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,13 +1,29 @@
+import React from 'react';
 import {
     View,
     Text,
     StyleSheet,
+    Button
 } from 'react-native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+export type RootStackParamList = {
+    Home: undefined;
+    About: undefined;
+    // Add other screens here as needed
+};
 
-export default function HomeScreen() {
+ export type ScreenProps = {
+    navigation: NativeStackNavigationProp<RootStackParamList, 'Home'>;
+};
+
+export default function HomeScreen({ navigation }: ScreenProps) {
     return (
         <View style={styles.container}>
             <Text style={{fontSize: 20}}>Home Screen</Text>
+            <Button 
+                title='Go to About' 
+                onPress={() => navigation.navigate('About')}
+            />
         </View>
     );
 }
@@ -18,6 +34,5 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: '#ecf0f1',
-
     },
 });
